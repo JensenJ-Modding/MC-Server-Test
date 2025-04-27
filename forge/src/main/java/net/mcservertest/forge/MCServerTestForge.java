f1b616d17febdd73defcb74d7104aefb7acd2f99
Jensen <jensenbradley@icloud.com>
2025-04-27 03:06:11 +0100

    FIx architectury dependency
    Removed architectury as a dependency in the code as we don't use any features from it. Also removed downloading the other dependencies from the workflow file.

--- forge/src/main/java/net/mcservertest/forge/MCServerTestForge.java
+++ forge/src/main/java/net/mcservertest/forge/MCServerTestForge.java
@@ -3,13 +3,11 @@ package net.mcservertest.forge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-import dev.architectury.platform.forge.EventBuses;
 import net.mcservertest.MCServerTest;
 
 @Mod(MCServerTest.MOD_ID)
 public class MCServerTestForge {
     public MCServerTestForge(FMLJavaModLoadingContext context) {
-        EventBuses.registerModEventBus(MCServerTest.MOD_ID, context.getModEventBus());
         MCServerTest.init();
     }
 }
